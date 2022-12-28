553cbed9eb28344dd2926fbd9b12928a27ab1e11
Betaperson <3016678@edison.k12.nj.us>
2022-12-28 11:41:36 -0500

    Revert "patched for camjam drive board"
    This reverts commit 90ee225283a38e27678a061c30e34a617f68eba3.

--- actuator-drv8835.py
+++ actuator-drv8835.py
@@ -1,71 +1,56 @@
-from gpiozero import CamJamKitRobot
-import time
+from pololu_drv8835_rpi import motors, MAX_SPEED
 
-MAX_SPEED = 70
+# init
+def init(default_speed=50):
+    motors.setSpeeds(0, 0)
+    set_speed(default_speed)
 
-robot = CamJamKitRobot()
+# throttle
+cur_speed = MAX_SPEED
 move_state = 0
 
-def set_speed(intSpeed):
+def get_max_speed():
+    return MAX_SPEED
+
+def set_speed(speed):
     global cur_speed
     global move_state
-    cur_speed = intSpeed/100
+    speed = int(MAX_SPEED * speed / 100)
+    cur_speed = min(MAX_SPEED, speed)
     if move_state == -1:
         rew()
     elif move_state == 1:
         ffw()
-    #speed = int(MAX_SPEED * intSpeed / 100)
-    #print(speed)
-    #cur_speed = min(MAX_SPEED, speed)
-    #print(cur_speed)
 
 def get_speed():
     return int(cur_speed * 100 / MAX_SPEED)
 
+def stop():
+    global move_state
+    motors.motor2.setSpeed(0)
+    move_state=0
+        
 def ffw():
     global move_state
-    motorspeed = (-cur_speed, 0)
-    robot.value = motorspeed
-    # motors.motor2.setSpeed(cur_speed)
-    move_state = 1
-    time.sleep(1)
+    motors.motor2.setSpeed(cur_speed)
+    move_state=1
 
 def rew():
     global move_state
-    motorspeed = (cur_speed, 0)
-    robot.value = motorspeed
-    # motors.motor2.setSpeed(-cur_speed)
-    move_state = -1
-    time.sleep(1)
+    motors.motor2.setSpeed(-cur_speed)
+    move_state=-1
+
+# steering
+def center():
+    motors.motor1.setSpeed(0)
 
 def left(speed=-1):
-    # motors.motor1.setSpeed(int(speed * MAX_SPEED))
-    motorspeed = (0, cur_speed * speed)
-    robot.value = motorspeed
-    time.sleep(3)
-
+    motors.motor1.setSpeed(int(speed*MAX_SPEED))
 
 def right(speed=1):
-    # motors.motor1.setSpeed(int(speed * MAX_SPEED))
-    motorspeed = (0, cur_speed * speed)
-    robot.value = motorspeed
-    time.sleep(3)
+    motors.motor1.setSpeed(int(speed*MAX_SPEED))
 
-def center():
-    motorspeed = (0, 0)
-
+# exit    
 def turn_off():
+    stop()
     center()
-    robot.stop()
-
-def init(default_speed=0.10416666666):
-    motorspeed = (0, 0)
-    set_speed(default_speed)
-
-
-# set_speed(50)
-# print(get_speed())
-#
-# for x in range(11):
-#     set_speed(x/10)
-#     ffw()
